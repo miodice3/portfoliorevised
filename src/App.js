47a9a7d26546c1c749b2ff9d9e7174fb0c0d5f3f
Michael Iodice <miodice3@gmail.com>
2021-04-13 07:14:25 -0600

    add modal conductor component within app render, link new file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Masthead from './Containers/masthead.js'
 import Portfolio from './Containers/portfolio.js'
 import About from './Containers/about.js'
 import Footer from './Containers/footer.js'
+import ModalConductor from './Modals/modalconductor.js'
 
 import './App.css';
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Portfolio />
       <About />
       <Footer />
+      {/* <ModalConductor /> */}
     </div>
   );
 }
