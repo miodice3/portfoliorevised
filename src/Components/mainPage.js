36bd3dbeac337503221649a1f5c145d110e6e15a
Michael Iodice <miodice3@gmail.com>
2021-04-17 09:26:58 -0600

    switch from modal to react router to expedite process, grouped masthead, portfolio and about in new component mainpage, removed modal folder and modal router

---
+++ src/Components/mainPage.js
@@ -0,0 +1,15 @@
+import Masthead from '../Containers/masthead.js'
+import Portfolio from '../Containers/portfolio.js'
+import About from '../Containers/about.js'
+
+function MainPage() {
+    return (
+        <>
+        <Masthead />
+        <Portfolio />
+        <About />
+        </>
+    );
+  }
+  
+  export default MainPage;
